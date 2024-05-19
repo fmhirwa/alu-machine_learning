4ede09f10817e410ce0a7bebbffeb4833cc9bbb6
fmhirwa <m.hirwa@alustudent.com>
2024-05-19 20:56:52 +0200

    Documentation fix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
-    '''
+    """
         This function calculates the shape of a matrix.
         Matrix: A list representing the matrix.
         Returns a list of integers that represents the shape of the matrix.
-    '''
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
